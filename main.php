0ca38d95e392d9d72bab312b0c0d270c4b41e0dd
luizguilhermefr <luizguilhermefr@gmail.com>
2017-06-12 16:13:31 -0300

    blur

--- main.php
+++ main.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Gerenciador RU | Main</title>
+    <title>Gerenciador RU | Principal</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.2.1.min.js"></script>
     <style type="text/css">
         body {
             background: url('img/imgs<?php echo rand(1,3);?>.jpg');
@@ -17,7 +18,7 @@
         <div class="panel-heading">
             <div class="input-group input-group-lg">
                 <span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-cutlery"></i></span>
-                <input type="text" class="form-control" placeholder="código" autofocus="autofocus"
+                <input id="entrada-codigo" type="text" class="form-control" placeholder="código" autofocus="autofocus"
                        aria-describedby="sizing-addon1">
                 <span class="input-group-btn"><button class="btn btn-default" type="button"><i
                                 class="glyphicon glyphicon-ok"></i></button></span>
@@ -48,7 +49,7 @@
                 </div>
             </div>
             <br/><br/><br/><br/><br/>
-            <div class="col-md-offset-1">
+            <div class="col-md-offset-1" id="valor">
 				<span class="label label-danger" style="font-size: 50px;">
 					R$ 0,00
 				</span>
@@ -56,5 +57,20 @@
         </div>
     </form>
 </div>
+<script>
+    $('#servidor,#aluno,#valor').hide();
+    $('#entrada-codigo').blur(function () {
+        var cod = $('#entrada-codigo').val();
+        if ((cod.length >= 4)) {
+            if (cod[0] === '1') {
+                $('#aluno').hide();
+                $('#servidor,#valor').show();
+            } else {
+                $('#servidor').hide();
+                $('#aluno,#valor').show();
+            }
+        }
+    });
+</script>
 </body>
 </html>

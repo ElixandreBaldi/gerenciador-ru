62995378e92154e475b6b0e2046441546a10cf06
luizguilhermefr <luizguilhermefr@gmail.com>
2017-07-28 19:09:00 -0300

    more refactor

--- Views/main.php
+++ Views/main.php
@@ -3,67 +3,56 @@
 <?php include_once('header.php') ?>
 <body>
 <div id="all">
-<?php if (isset($success)) { ?>
+<?php if (isset($_SESSION['consume_success'])) { ?>
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
-        <?php if ($success) { ?>
+        <?php if ($_SESSION['consume_success']) { ?>
             <div class="alert alert-success">
                 <strong>Sucesso!</strong> Consumo efetuado com sucesso!
             </div>
         <?php } else { ?>
             <div class="alert alert-danger">
-                <strong>Erro!</strong> Não foi possível inserir o usuário.
+                <strong>Erro!</strong>
                 <?php
-                if (isset($errors)) {
-                    foreach ($errors as $error) {
+                if (isset($_SESSION['consume_error'])) {
+                    foreach ($_SESSION['consume_error'] as $error) {
                         echo "<br>" . $error;
                     }
+                    unset($_SESSION['consume_error']);
                 }
                 ?>
             </div>
         <?php } ?>
         </div>
     </div>
-    <script>
-        function disable_f5(e) {
-            if ((e.which || e.keyCode) == 116) {
-                e.preventDefault();
-            }
-        }
-
-        $(document).ready(function () {
-            $(document).bind("keydown", disable_f5);
-        });
-    </script>
     <?php
-        }
+        unset ($_SESSION['consume_success']);}
+        if (!isset($_SESSION['to_consume'])) {
     ?>
-    <form method="POST" action="consumirController.php">
+    <form method="POST" action="consumir.php">
         <div class="panel-heading">
             <legend>Consumir Refeição</legend>
             <div class="input-group input-group-lg">
                 <span class="input-group-addon" id="sizing-addon1"><i class="glyphicon glyphicon-cutlery"></i></span>
-                <input name="novoRegistroConsumo" id="entrada-codigo" type="text" class="form-control" placeholder="Insira o cartão do cliente" autofocus="autofocus"
+                <input name="novoRegistroConsumo" id="entrada-codigo" class="form-control" placeholder="Insira o cartão do cliente" autofocus="autofocus"
                        aria-describedby="sizing-addon1">
-                <span class="input-group-btn"><button type="submit" class="btn btn-default"><i
+                <span class="input-group-btn"><button class="btn btn-default"><i
                                 class="glyphicon glyphicon-ok"></i></button></span>
             </div>
         </div>
     </form>
+    <?php } else {
+        if ($_SESSION['to_consume']['nivel_str'] == 'registro_academico') {?>
         <div class="panel-body">
-            <?php
-                if(isset($flagSearch)) {
-                    if ($nivel == "registro_academico") {
-            ?>
             <div id="aluno">
                 <legend>Aluno</legend>
                 <div class="form-group col-md-5">
                     <label for="ra">Registro Acadêmico:</label>
-                    <input type="text" value="<?=$nomeUsuarioRecarga;?>" class="form-control" readonly="readonly" name="ra"/>
+                    <input value="<?=$_SESSION['to_consume']['registro'];?>" class="form-control" readonly name="ra"/>
                 </div>
                 <div class="form-group col-md-7">
                     <label for="ra">Nome:</label>
-                    <input type="text" value="<?=$registro;?>" class="form-control" readonly="readonly" name="ra"/>
+                    <input value="<?=$_SESSION['to_consume']['nome'];?>" class="form-control" readonly name="ra"/>
                 </div>
             </div>
             <?php
@@ -73,11 +62,11 @@
                 <legend>Servidor</legend>
                 <div class="form-group col-md-5">
                     <label for="ra">Registro Universitáro:</label>
-                    <input type="text" value="<?=$nomeUsuarioRecarga;?>" class="form-control" readonly="readonly" name="ra"/>
+                    <input value="<?=$_SESSION['to_consume']['registro'];?>" class="form-control" readonly="readonly" name="ra"/>
                 </div>
                 <div class="form-group col-md-7">
                     <label for="ra">Nome:</label>
-                    <input type="text" value="<?=$registro;?>" class="form-control" readonly="readonly" name="ra"/>
+                    <input value="<?=$_SESSION['to_consume']['nome'];?>" class="form-control" readonly="readonly" name="ra"/>
                 </div>
             </div>
             <?php
@@ -85,18 +74,17 @@
             ?>
             <br/><br/><br/><br/><br/>
             <div class="col-md-offset-1" id="valor">
-                <form action="consumirController.php" method="POST">
-                    <input type="hidden" value="<?=$registro;?>" name="consumidor">
-                    <input type="hidden" value="<?=$nivel;?>" name="nivel">
-                    <button type="submit" href="main.php" class="btn button">
+                <form action="consumir.php" method="POST">
+                    <input type="hidden" value="<?=$_SESSION['to_consume']['id'];?>" name="consumidor">
+                    <button href="main.php" class="btn button">
                         <span class="label label-danger" style="font-size: 50px;">
-                            <?="R$ ".$valorRefeicaoStr;?> <i class="glyphicon glyphicon-refresh"></i>
+                            <?="R$ ".$_SESSION['to_consume']['valor'];?> <i class="glyphicon glyphicon-refresh"></i>
                         </span>
                     </button>
                 </form>
             </div>
             <?php
-                }
+            unset($_SESSION['to_consume']); }
             ?>
         </div>
 </div>
--- consumir.php
+++ consumir.php
@@ -1,20 +1,71 @@
 <?php
 
+include_once('autoload.php');
+
 if (! isset($_SESSION['usr']) || ! $loggedUser = Usuario::find($_SESSION['usr'])) {
     header('Location: login.php');
+    die;
 }
-else if (! $admin = $loggedUser->isAdmin()) {
+
+if (! $admin = $loggedUser->isAdmin()) {
     header('Location: historico.php');
+    die;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $register = isset($_POST['code']) ? $_POST['code'] : null;
-    $errors = [];
-    $registerUser = Usuario::findByRegister($register);
-    if (!$registerUser) {
-        array_push($errors, "Nenhum usuário encontrado com o registro {$register}.");
-        $success = false;
-    }
+    if (isset($_POST['novoRegistroConsumo'])) {
 
-    var_dump($register);
+        $registroUsuarioConsumo = $_POST['novoRegistroConsumo'];
+
+        if ($_POST['novoRegistroConsumo'][0] == 3) {
+            $nivelStr = "registro_academico";
+        } else {
+            $nivelStr = "registro_universitario";
+        }
+
+        $usuarioConsumo = Usuario::search()
+            ->whereEqual($nivelStr, $registroUsuarioConsumo)
+            ->limit(1)
+            ->run();
+
+        if (count($usuarioConsumo) < 1) {
+            $_SESSION['consume_success'] = false;
+            $_SESSION['consume_error'] = [
+                'Nenhum usuário encontrado.',
+            ];
+        } else {
+            $_SESSION['to_consume'] = [
+                'id' => $usuarioConsumo[0]['id'],
+                'nome' => $usuarioConsumo[0]['usuario'],
+                'nivel' => $usuarioConsumo[0]['nivel'],
+                'nivel_str' => $nivelStr,
+                'valor' => $usuarioConsumo[0]['nivel'] == 2 ? 2.5 : 6.0,
+                'registro' => $usuarioConsumo[0][$nivelStr]
+            ];
+        }
+
+        header('Location: main.php');
+    } else {
+        $consumidor = (isset ($_POST['consumidor'])) ? $_POST['consumidor'] : null;
+
+        $usuario = Usuario::find($consumidor);
+
+        if (!$usuario) {
+            $_SESSION['consume_success'] = false;
+            $_SESSION['consume_error'] = [
+                'Nenhum usuário encontrado.',
+            ];
+        } else {
+            $valor = $usuario->getNivel() == 2 ? -2.5 : -6.0;
+            $transacao = Transacao::insert()
+                ->values(["valor" => $valor,
+                    "usuario_id" => $usuario->getId(),
+                    "criado_em" => date('Y-m-d H:i:s')])
+                ->run();
+            $_SESSION['consume_success'] = true;
+        }
+
+        header('Location: main.php');
+    }
 }
+
